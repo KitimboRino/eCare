668b372b1a9d641bace7da7ccb93e06a801b1ca8
Kitimbo Rino <kitimborinoemma0580@gmail.com>
2024-08-27 13:52:53 +0300

    feat: appwrite setup for database

--- app/page.tsx
+++ app/page.tsx
@@ -15,12 +15,12 @@ export default function Home() {
             height={1000}
             width={1000}
             alt="logo"
-            className="mb-12 h-10 w-fit"
+            className="mb-20 h-10 w-fit"
           />
           <PatientForm />
 
           <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">
+            <p className="text-xs justify-items-end text-dark-600 xl:text-left">
               © 2024 eCare
             </p>
             <Link href='/?admin=true' className="text-green-500">
